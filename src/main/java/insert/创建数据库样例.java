bc3e5c0b86dd67698584d7676deeec75f582e910
ljyTlm <970955947@qq.com>
2019-07-07 21:43:40 +0800

    create database `mysql` character set utf collate utf8_general_ci

---
+++ src/main/java/insert/创建数据库样例.java
@@ -0,0 +1,12 @@
+package insert;
+
+public class 创建数据库样例 {
+}
+/*
+    1.create关键字
+        命令格式：create database [数据库名] character set utf8 collate utf8_general_ci;
+        创建数据库并设置为编码为utf-8
+
+
+
+ */
