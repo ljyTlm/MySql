eb0b70414726d9d427783c69c2b748b73db1e158
ljyTlm <970955947@qq.com>
2019-07-07 22:08:49 +0800

    insert into `myTable` () values () insert into `myTable` select * from `otherTable`

---
+++ src/main/java/insert/插入数据样例.java
@@ -0,0 +1,14 @@
+package insert;
+
+public class 插入数据样例 {
+}
+/*
+    1.insert关键字
+        命令格式：insert into [表名] ([字段1],[字段2],[字段3])
+                values ([v1], [v2], [v3])
+                       ,([v1], [v2], [v3])
+        可以插入多行数据 只要指定一下列就好
+
+        复制数据：insert into [表名] select * from [表名]
+
+ */
