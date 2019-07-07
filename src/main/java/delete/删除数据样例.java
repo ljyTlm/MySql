0a6c448a1561d8140d48bf7e8a188e1dd9ee3c64
ljyTlm <970955947@qq.com>
2019-07-07 19:54:25 +0800

    整理了delete关键字

---
+++ src/main/java/delete/删除数据样例.java
@@ -0,0 +1,13 @@
+package delete;
+
+public class 删除数据样例 {
+}
+/*
+    1.delete关键字
+        命令格式：delete from [表名] where [字段名] = [条件]
+        条件的搭配可以让我们删除一条或多条数据
+        delete from ？where ？ like ？
+
+
+
+ */
