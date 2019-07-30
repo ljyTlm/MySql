a7a6d11a5380c7100c4756c6c4a6f0784827bf2f
ljyTlm <970955947@qq.com>
2019-07-30 16:27:25 +0800

    上次漏下的

---
+++ src/main/java/insert/添加字段样例.java
@@ -0,0 +1,10 @@
+package insert;
+
+public class 添加字段样例 {
+}
+/**
+ *      1.alter关键字
+ *          命令格式：alter table [tab_name] add [column_name] [parameters]
+ *          样例：
+ *              alter table tab add name int(10)
+ * */
