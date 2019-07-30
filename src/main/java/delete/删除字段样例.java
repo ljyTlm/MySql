cf1c8c3df6c8d91ba81a6dcc2527118d03e71439
ljyTlm <970955947@qq.com>
2019-07-30 16:25:05 +0800

    alter table tab add name varchar(100) alter table tab drop name alter table tab add index p(name) alter table tab drop index p

---
+++ src/main/java/delete/删除字段样例.java
@@ -0,0 +1,11 @@
+package delete;
+
+public class 删除字段样例 {
+}
+/**
+ *      1.alter关键字 + drop 关键字
+ *          命令格式：alter table [tab_name] drop [column_name]
+ *          样例：
+ *              alter table tab drop name
+ *
+ * */
---
+++ src/main/java/delete/删除索引样例.java
@@ -0,0 +1,10 @@
+package delete;
+
+public class 删除索引样例 {
+}
+/**
+ *     1.alter 关键字 + drop 关键字
+ *          命令格式： alter table [tab_name] drop [key_type] [key_name]
+ *          样例：
+ *              alter table tab drop index p
+ * */
