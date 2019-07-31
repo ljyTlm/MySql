78845dbe93439f9731feb337cfa5268e1ffbe22d
ljyTlm <970955947@qq.com>
2019-07-31 11:44:18 +0800

    alter table tab engine=InnoDB

---
+++ src/main/java/updata/重建表样例.java
@@ -0,0 +1,12 @@
+package updata;
+
+public class 重建表样例 {
+}
+/**
+ *      1.alter关键字
+ *          命令格式：alter table [tab_name] engine=InnoDB
+ *          mysql会将指定的表 复制出来 并替代原先的表 用于整理空间
+ *          样例：
+ *              alter table tab engine=InnoDB
+ *
+ * */
